ad50140246c6dfe0a6e9a229a0c7a2af4402b521
Sahil <sahilahmed466@gmail.com>
2024-07-05 20:14:19 +0530

    Invitation Feature

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -4,6 +4,7 @@ import { ServerWithMembersWithProfile } from "@/types";
 import { MemberRole } from "@prisma/client";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { ChevronDown, LogOut, PlusCircle, Settings, Trash, UserPlus, Users } from "lucide-react";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerHeaderProps {
     server: ServerWithMembersWithProfile;
@@ -14,6 +15,8 @@ export const ServerHeader = ({
     server,
     role,
 }: ServerHeaderProps) => {
+
+    const { onOpen } = useModal();
 
     const isAdmin = role === MemberRole.ADMIN;
     const isModerator = isAdmin || role === MemberRole.MODERATOR;
@@ -31,7 +34,10 @@ export const ServerHeader = ({
             </DropdownMenuTrigger>
             <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
                 {isModerator && (
-                    <DropdownMenuItem className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem
+                        onClick={() => onOpen("invite", { server })}
+                        className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
+                    >
                         Invite People
                         <UserPlus className="h-4 w-4 ml-auto" />
                     </DropdownMenuItem>
@@ -63,7 +69,7 @@ export const ServerHeader = ({
                 )}
 
                 {isAdmin && (
-                    <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem className="text-red-500 px-3 py-2 text-sm cursor-pointer">
                         Delete Server
                         <Trash className="h-4 w-4 ml-auto" />
                     </DropdownMenuItem>
