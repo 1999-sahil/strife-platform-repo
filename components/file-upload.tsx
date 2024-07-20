00e7fb21884f02777eb453f4ccc2cb601876be0b
Sahil <sahilahmed466@gmail.com>
2024-07-20 18:11:08 +0530

    Message Attachment image/pdf

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { UploadDropzone } from "@/lib/uploadThing";
-import "@uploadthing/react/styles.css";
-import { X } from "lucide-react";
 import Image from "next/image";
+import "@uploadthing/react/styles.css";
+
+import { UploadDropzone } from "@/lib/uploadThing";
+import { FileCheck2, X } from "lucide-react";
 
 interface FileUploadProps {
     onChange: (url?: string) => void;
@@ -33,6 +34,30 @@ export const FileUpload = ({
                     type="button"
                     onClick={() => onChange("")}
                     className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+                >
+                    <X className="h-4 w-4" />
+                </button>
+            </div>
+        )
+    }
+
+    // see the pdf which we uploaded.
+    if (value && fileType === "pdf") {
+        return (
+            <div className="flex items-center relative p-2 mt-2 rounded-md bg-background/10">
+                <FileCheck2 className="h-10 w-10 fill-emerald-200 stroke-emerald-400" />
+                <a
+                    href={value}
+                    target="_balnk"
+                    rel="noopener noreferrer"
+                    className="ml-2 text-sm text-emerald-500 dark:text-emerald-400 hover:underline"
+                >
+                    {value}
+                </a>
+                <button
+                    type="button"
+                    onClick={() => onChange("")}
+                    className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
                 >
                     <X className="h-4 w-4" />
                 </button>
