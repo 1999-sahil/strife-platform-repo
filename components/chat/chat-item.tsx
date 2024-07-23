5aa0f1c6d712252599ff0d9cc412c96ea9a0efdb
Sahil <sahilahmed466@gmail.com>
2024-07-23 16:23:49 +0530

    Chat Item Component with edit/delete content

---
+++ components/chat/chat-item.tsx
@@ -0,0 +1,146 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+import { Member, MemberRole, Profile } from "@prisma/client";
+import { Crown, FilePenLine, FileCheck2, ShieldCheck, Trash, UserRoundCheck } from "lucide-react";
+
+import { UserAvatar } from "@/components/user-avatar";
+import { ActionTooltip } from "@/components/action-tooltip";
+import { cn } from "@/lib/utils";
+
+interface ChatItemProps {
+    id: string;
+    content: string;
+    member: Member & {
+        profile: Profile
+    };
+    timestamp: string;
+    fileUrl: string | null;
+    deleted: boolean;
+    currentMember: Member;
+    isUpdated: boolean;
+    socketUrl: string;
+    socketQuery: Record<string, string>;
+};
+
+const roleIconMap = {
+    "GUEST": <UserRoundCheck className="h-4 w-4 ml-2 text-gray-800 dark:text-gray-500" />,
+    "MODERATOR": <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500" />,
+    "ADMIN" : <Crown className="h-4 w-4 ml-2 text-yellow-600" />, 
+};
+
+export const ChatItem = ({
+    id,
+    content,
+    member,
+    timestamp,
+    fileUrl,
+    deleted,
+    currentMember,
+    isUpdated,
+    socketUrl,
+    socketQuery,
+}: ChatItemProps) => {
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [isDeleting, setIsDeleting] = useState(false);
+
+    const fileType = fileUrl?.split(".").pop();
+
+    const isAdmin = currentMember.role === MemberRole.ADMIN;
+    const isModerator = currentMember.role === MemberRole.MODERATOR;
+    const isOwner = currentMember.role === member.id;
+    const canDeleteMessage = !deleted && (isAdmin || isModerator || isOwner);
+    const canEditMessage = !deleted && isOwner && !fileUrl;
+    const isPdf = fileType === "pdf" && fileUrl;
+    const isImage = !isPdf && fileUrl;
+
+    return (
+        <div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
+            <div className="group flex gap-x-2 items-start w-full">
+                <div className="cursor-pointer hover:drop-shadow-md transition">
+                    <UserAvatar src={member.profile.imageUrl} />
+                </div>
+                <div className="flex flex-col w-full">
+                    <div className="flex items-center gap-x-2">
+                        <div className="flex items-center">
+                            <p className="font-semibold text-sm hover:underline cursor-pointer">
+                                {member.profile.name}
+                            </p>
+                            <ActionTooltip label={member.role}>
+                                {roleIconMap[member.role]}
+                            </ActionTooltip>
+                        </div>
+                        <span className="text-xs text-zinc-500 dark:text-zinc-400">
+                            {timestamp}
+                        </span>
+                    </div>
+
+                    {isImage && (
+                        <a
+                            href={fileUrl}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="relative aspect-square rounded-md mt-2 overflow-hidden border flex items-center bg-secondary h-48 w-48"
+                        >
+                            <Image
+                                src={fileUrl}
+                                alt={content}
+                                fill
+                                className="object-cover"
+                            />
+                        </a>
+                    )}
+
+                    {isPdf && (
+                        <div className="flex items-center relative p-2 mt-2 rounded-md bg-background/10">
+                            <FileCheck2 className="h-10 w-10 fill-emerald-200 stroke-emerald-400" />
+                            <a
+                                href={fileUrl}
+                                target="_balnk"
+                                rel="noopener noreferrer"
+                                className="ml-2 text-sm text-emerald-500 dark:text-emerald-400 hover:underline"
+                            >
+                                PDF File
+                            </a>
+                        </div>
+                    )}
+
+                    {!fileUrl && !isEditing && (
+                        <p className={cn(
+                                "text-sm text-zinc-600 dark:text-zinc-300",
+                                deleted && "italic text-zinc-500 dark:text-zinc-400 text-xs mt-1"
+                            )}
+                        >
+                            {content}
+                            {isUpdated && !deleted && (
+                                <span className="text-[10px] mx-2 text-zinc-500 dark:text-zinc-400">
+                                    (edited)
+                                </span>
+                            )}
+                        </p>
+                    )}
+                </div>
+            </div>
+
+            {canDeleteMessage && (
+                <div className="hidden group-hover:flex items-center gap-x-2 absolute p-1 -top-2 right-5 bg-white dark:bg-zinc-800 border rounded-sm">
+                    {canEditMessage && (
+                        <ActionTooltip label="Edit">
+                            <FilePenLine 
+                                //onClick={() => setIsEditing(true)}
+                                className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition"
+                            />
+                        </ActionTooltip>
+                    )}
+                    <ActionTooltip label="Delete">
+                        <Trash className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-red-500 transition" />
+                    </ActionTooltip>
+                </div>
+            )}
+        </div>
+    )
+};
+
