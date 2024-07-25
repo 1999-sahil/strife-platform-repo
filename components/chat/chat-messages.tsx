f69fcd2f239c794f60161ffcb2efde8788cd0ead
Sahil <sahilahmed466@gmail.com>
2024-07-25 14:53:32 +0530

    Chat Scroll Hook to display messages

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Fragment } from "react";
+import { Fragment, useRef, ElementRef } from "react";
 import { format } from "date-fns";
 
 import { Member, Message, Profile } from "@prisma/client";
@@ -11,6 +11,7 @@ import { useChatSocket } from "@/hooks/use-chat-socket";
 
 import { ChatWelcome } from "@/components/chat/chat-welcome";
 import { ChatItem } from "@/components/chat/chat-item";
+import { useChatScroll } from "@/hooks/use-chat-scroll";
 
 const DATE_FORMAT = "d MMM yyyy, HH:mm";
 
@@ -48,6 +49,10 @@ export const ChatMessages = ({
     const addKey = `chat:${chatId}:messages`;
     const updateKey = `chat:${chatId}:messages:update`;
 
+    // For Chat-Sroll-Hook
+    const chatRef = useRef<ElementRef<"div">>(null);
+    const bottomRef = useRef<ElementRef<"div">>(null);
+
     const {
         data,
         fetchNextPage,
@@ -63,6 +68,15 @@ export const ChatMessages = ({
 
     // chat-socket-hook for real time communication
     useChatSocket({ queryKey, addKey, updateKey });
+
+    // chat-scroll-hook for fetching previous messages when scrolled
+    useChatScroll({
+        chatRef,
+        bottomRef,
+        loadMore: fetchNextPage,
+        shouldLoadMore: !isFetchingNextPage && !!hasNextPage,
+        count: data?.pages?.[0]?.items?.length ?? 0,
+    });
 
     if (status === "pending") {
         return (
@@ -87,13 +101,29 @@ export const ChatMessages = ({
     }
 
     return (
-        <div className="flex-1 flex flex-col py-4 overflow-y-auto">
-            <div className="flex-1" />
+        <div ref={chatRef} className="flex-1 flex flex-col py-4 overflow-y-auto">
+            {!hasNextPage && <div className="flex-1" />}
 
-            <ChatWelcome
-                type={type}
-                name={name}
-            />
+            {!hasNextPage && (
+                <ChatWelcome
+                    type={type}
+                    name={name}
+                />
+            )}
+
+            {/** Display loader or button before displaying message */}
+            {hasNextPage ? (
+                <div className="flex justify-center">
+                    <Loader2 className="h-6 w-6 text-zinc-500 animate-spin my-4" />
+                </div>
+            ) : (
+                <button
+                    onClick={() => fetchNextPage()}
+                    className="text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 text-xs my-4 transition"
+                >
+                    Load previous messages...
+                </button>
+            )}
 
             {/** Displaying message which we write on input bar */}
             <div className="flex flex-col-reverse mt-auto">
@@ -117,6 +147,7 @@ export const ChatMessages = ({
                     </Fragment>
                 ))}
             </div>
+            <div ref={bottomRef} />
         </div>
     )
 };
